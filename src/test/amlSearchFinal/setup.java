d8c4efbcc0010005af1af65bf4d0d4cc7b4e6555
xyz <xyz@DESKTOP-TJE3U23>
2020-11-03 14:56:17 +0530

    commit done

---
+++ src/test/amlSearchFinal/setup.java
@@ -0,0 +1,28 @@
+	package amlSearchFinal;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class setup {
+static WebDriver driver;
+	
+	
+	
+	
+	
+	public static void main(String args[]) throws FileNotFoundException, IOException {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\xyz\\eclipse-workspace\\amlSearch\\chromedriver.exe");
+		//ExcelDataConfig excel = new ExcelDataConfig("C:\\kyc data\\kyc.xlsx");
+			
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://dashboard.stage.kychub.com/login");
+
+}
+}
